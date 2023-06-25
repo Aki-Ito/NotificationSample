36efd44e57ad6735db586316153281b6ffaa755a
Aki-Ito <60352606+Aki-Ito@users.noreply.github.com>
2023-06-25 09:59:44 +0900

    ppp

--- Notification/AppDelegate.swift
+++ Notification/AppDelegate.swift
@@ -6,31 +6,55 @@
 //
 
 import UIKit
+//TODO: 追加
+import UserNotifications
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+//TODO: UNUserNotificationCenterDelegateを追加
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // MARK: 通知の許可をリクエストする
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted: Bool, error: Error?) in
+            print("許可されたか: \(granted)")
+        }
+        // MARK: UNUserNotificationCenterDelegateを指定する
+        UNUserNotificationCenter.current().delegate = self
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
+    
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    //TODO: 追加
+    // MARK: UNUserNotificationDelegateを実装する
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler(
+            [
+                UNNotificationPresentationOptions.banner,
+                UNNotificationPresentationOptions.list,
+                UNNotificationPresentationOptions.sound,
+                UNNotificationPresentationOptions.badge
+            ]
+        )
+    }
+    //TODO: 追加
+    // バックグランドで通知を受け取った際に呼ばれるメソッド
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+    
+    
 }
 
--- Notification/ViewController.swift
+++ Notification/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+//TODO: 追加
+import NotificationCenter
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //TODO: 追加
+    @IBAction func addNotification(){
+        let datePickerTime = datePicker.date
+        
+        let notificationTitle: String = "通知のタイトル"
+        let calendar: Calendar = Calendar.current
+        //MARK: datePickerから取得した日付や時間を反映する(今回は時間と分のみ)
+        let trigger: UNCalendarNotificationTrigger = UNCalendarNotificationTrigger(dateMatching: calendar.dateComponents([.hour, .minute], from: datePickerTime), repeats: false)
+        
+        // MARK: 通知の中身を設定
+        let content: UNMutableNotificationContent = UNMutableNotificationContent()
+        content.title = notificationTitle
+        content.sound = UNNotificationSound.default
+        content.badge = 1
+        
+        // MARK: 通知のリクエストを作成
+        let request: UNNotificationRequest = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        // MARK: 通知のリクエストを実際に登録する
+        UNUserNotificationCenter.current().add(request) { (error: Error?) in
+            // エラーが存在しているかをif文で確認している
+            if error != nil {
+                // MARK: エラーが出た時の処理をかく
+                print("error")
+            } else {
+                // MARK: 成功した時の処理をかく
+                print("success")
+            }
+        }
+        
+        
+    }
+    
+    
 }
 
